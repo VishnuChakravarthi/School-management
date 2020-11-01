cf1ec3e8d3436067e6ce81faea38092149c333f5
Vishnu Chakravarthi <vishchakravarthy18@gmail.com>
2020-11-01 18:36:37 +0530

    Update mongoose.js

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 
 mongoose.connect(
-  "mongodb+srv://vishnu:vishnu10@cluster0.sgs7u.mongodb.net/test",
+  "mongodb+srv://<username>:<password>@cluster0.sgs7u.mongodb.net/test",
   {
     useNewUrlParser: true,
     useFindAndModify: false,
